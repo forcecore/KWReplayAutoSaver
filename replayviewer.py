0503b50a5cefcdd83ab4cbed2b61ef2a7cedc779
BoolBada <ssanzing@gmail.com>
2014-10-20 20:15:14 +0900

    Adding replay viewer

--- replayviewer.py
+++ replayviewer.py
@@ -10,15 +10,20 @@ class ReplayViewer( wx.Frame ) :
 		box1 = wx.BoxSizer(wx.VERTICAL)
 		box2 = wx.BoxSizer(wx.VERTICAL)
 
-		panel1 = wx.Panel(self,-1, style=wx.SUNKEN_BORDER)
-		panel2 = wx.Panel(self,-1, style=wx.SUNKEN_BORDER)
-		panel3 = wx.Panel(self,-1, style=wx.SUNKEN_BORDER)
+		panel2 = wx.Panel( self, -1, style=wx.SUNKEN_BORDER )
+		panel3 = wx.Panel( self, -1, style=wx.SUNKEN_BORDER )
 
-		panel1.SetBackgroundColour("RED")
+		# player list
+		self.list_ctrl = wx.ListCtrl( self, size=(-1,200), style=wx.LC_REPORT )
+		self.list_ctrl.InsertColumn( 0, 'Team' )
+		self.list_ctrl.InsertColumn( 1, 'Name' )
+		self.list_ctrl.InsertColumn( 2, 'Faction' )
+		self.list_ctrl.InsertColumn( 3, 'Color' )
+
 		panel2.SetBackgroundColour("GREEN")
 		panel3.SetBackgroundColour("BLUE")
 
-		box1.Add(panel1, 1, wx.EXPAND)
+		box1.Add( self.list_ctrl, 1, wx.EXPAND )
 		box1.Add( box2, 2, wx.EXPAND )
 		box2.Add(panel2, 1, wx.EXPAND)
 		box2.Add(panel3, 2, wx.EXPAND)
