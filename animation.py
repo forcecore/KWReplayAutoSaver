c7b35b1c6922166e1d40db522c116a76d97da899
BoolBada <ssanzing@gmail.com>
2014-12-14 22:46:59 +0900

    Kinda works, not to my full satisfaction, though

--- animation.py
+++ animation.py
@@ -9,29 +9,13 @@ from args import Args
 
 
 
-class PosViewer( wx.Frame ) :
-	def __init__( self, parent, args, maps_zip='maps.zip' ) :
-		super().__init__( parent, title='Replay Movement Viewer', size=(500,500) )
-		self.parent = parent
-		self.args = args
-		self.MAPS_ZIP = maps_zip
+class MiniMap( MapView ) :
+	def __init__( self, parent, maps, mcmap, size=(200,200), pos=(0,0) ) :
+		super().__init__( parent, maps, mcmap, size=size, pos=pos )
 
+		self.t = 0
+		self.posa = None
 		self.kwr = None
-		self.pos_analyzer = None
-		self.length = 100 # default
-
-		self.movementss = None
-		self.buildingss = None
-		# buildingss[ pid ] = buildings
-		# buildings = [ (t1, loc1), (t2, loc2) ]
-		# loc = (x, y) pair.
-
-		self.map_view = None
-		self.map_bmp = None # remember how it was before drawing anything on it.
-		self.slider = None
-		self.time = None
-		self.do_layout()
-		self.event_bindings()
 
 		self.colors = None
 		self.bldg_colors = None
@@ -39,8 +23,8 @@ class PosViewer( wx.Frame ) :
 		self.x_offset = 0
 		self.y_offset = 0
 		self.make_palette()
-	
 
+		self.Bind( wx.EVT_PAINT, self.OnPaint )
 
 	def make_palette( self ) :
 		self.colors = [
@@ -62,7 +46,136 @@ class PosViewer( wx.Frame ) :
 				'#007f7f', # Cyan
 				'#7f667f', # Pink
 			]
+	
+	def show( self, kwr, scale=True, watermark=True ) :
+		super().show( self.kwr, scale=scale, watermark=watermark )
 
+
+
+	def calc_scale_factor( self ) :
+		# get X and Y.
+		X = 0
+		Y = 0
+		for commands in self.posa.commandss :
+			for cmd in commands :
+				if cmd.cmd_id == 0x8A :
+					X = max( X, cmd.x1 )
+					X = max( X, cmd.x2 )
+					Y = max( Y, cmd.y1 )
+					Y = max( Y, cmd.y2 )
+				else :
+					X = max( X, cmd.x )
+					Y = max( Y, cmd.y )
+
+		bmp = self.Bitmap
+		W = bmp.GetWidth()
+		H = bmp.GetHeight()
+
+		fx = W/X
+		fy = H/Y
+
+		self.scale = max( fx, fy )
+
+
+	def draw_dot( self, dc, x, y, color ) :
+		x = int( self.scale * x ) + self.x_offset
+		y = dc.Y - int( self.scale * y ) + self.y_offset
+		dc.SetPen( wx.Pen( color ) )
+		dc.DrawLine( x-3, y, x+3, y )
+		dc.DrawLine( x, y-3, x, y+3 )
+	
+	def draw_building( self, dc, x, y, color ) :
+		x = int( self.scale * x ) + self.x_offset
+		y = dc.Y - int( self.scale * y ) + self.y_offset
+		dc.SetPen( wx.Pen( color ) )
+		dc.SetBrush( wx.Brush( color ) )
+		dc.DrawCircle( x, y, 3 )
+
+
+
+	def draw_positions( self, t ) :
+		self.t = t
+		self.Refresh()
+
+	def OnPaint( self, evt ) :
+		if not self :
+			return
+		if self.posa == None :
+			return
+
+		bmp = self.Bitmap # internal data! careful not to modify this!
+
+		#self.unit_view.show( self.kwr, scale=False ) # remove previous dots.
+		#self.minimap.SetBitmap( self.map_bmp )
+		#self.SetBitmap( self.bmp )
+		dc = wx.PaintDC( self )
+		dc.DrawBitmap( bmp, 0, 0 )
+		#trans_brush = wx.Brush( wx.BLACK, style=wx.BRUSHSTYLE_TRANSPARENT )
+		#dc.SetBackground( trans_brush )
+		#dc.Clear()
+		#bmp = wx.Bitmap( self.map_bmp )
+		#dc = wx.MemoryDC( bmp )
+		null, dc.Y = bmp.GetSize()
+
+		# draw buildings
+		for i in range( 0, self.t ) :
+			commands = self.posa.structures[ i ]
+
+			for cmd in commands :
+				pid = cmd.player_id
+				player = self.kwr.players[ pid ]
+				if not player.is_player() :
+					continue
+				self.draw_building( dc, cmd.x, cmd.y, self.bldg_colors[pid] )
+
+
+		# Draw movement dots
+		for i in range( max( 0, self.t-10 ), self.t ) :
+			commands = self.posa.commands[ i ]
+
+			for cmd in commands :
+				pid = cmd.player_id
+				player = self.kwr.players[ cmd.player_id ]
+				if not player.is_player() :
+					continue
+
+				if cmd.cmd_id == 0x8A : # wormhole
+					#print( "0x%08X" % cmd.cmd_id )
+					self.draw_dot( dc, cmd.x1, cmd.y1, self.colors[pid] )
+					self.draw_dot( dc, cmd.x2, cmd.y2, self.colors[pid] )
+				else :
+					self.draw_dot( dc, cmd.x, cmd.y, self.colors[pid] )
+
+		del dc
+		#self.SetBitmap( bmp )
+
+
+
+class PosViewer( wx.Frame ) :
+	def __init__( self, parent, args, maps_zip='maps.zip' ) :
+		super().__init__( parent, title='Replay Movement Viewer', size=(500,500) )
+		self.parent = parent
+		self.args = args
+		self.MAPS_ZIP = maps_zip
+
+		self.kwr = None
+		self.pos_analyzer = None
+		self.length = 100 # default
+
+		self.movementss = None
+		self.buildingss = None
+		# buildingss[ pid ] = buildings
+		# buildings = [ (t1, loc1), (t2, loc2) ]
+		# loc = (x, y) pair.
+
+		self.minimap = None
+		#self.map_bmp = None # remember how it was before drawing anything on it.
+		self.slider = None
+		self.time = None
+		self.do_layout()
+		self.event_bindings()
+
+	
 
 	def create_top_panel( self, parent ) :
 		panel = wx.Panel( parent )
@@ -71,12 +184,12 @@ class PosViewer( wx.Frame ) :
 		sizer = wx.BoxSizer( wx.HORIZONTAL )
 
 		# map view
-		self.map_view = MapView( panel, self.MAPS_ZIP, self.args.mcmap, size=(400,400) )
+		self.minimap = MiniMap( panel, self.MAPS_ZIP, self.args.mcmap, size=(400,400) )
 
 		# map control panel
 		panel_map_ctrl = wx.Panel( self )
 		panel_map_ctrl.SetBackgroundColour( (255,0,0) )
-		sizer.Add( self.map_view, 0, wx.ALIGN_LEFT|wx.ALIGN_TOP )
+		sizer.Add( self.minimap, 0, wx.ALIGN_LEFT|wx.ALIGN_TOP )
 		sizer.Add( panel_map_ctrl, 1, wx.EXPAND )
 
 		panel.SetSizer( sizer )
@@ -106,100 +219,8 @@ class PosViewer( wx.Frame ) :
 	def on_scroll( self, evt ) :
 		t = evt.GetPosition()
 		self.time.SetLabel( time_code2str( t ) )
-		self.draw_positions( t )
+		self.minimap.draw_positions( t )
 	
-
-
-	def draw_dot( self, dc, x, y, color ) :
-		x = int( self.scale * x ) + self.x_offset
-		y = dc.Y - int( self.scale * y ) + self.y_offset
-		dc.SetPen( wx.Pen( color ) )
-		dc.DrawLine( x-2, y, x+2, y )
-		dc.DrawLine( x, y-2, x, y+2 )
-	
-	def draw_building( self, dc, x, y, color ) :
-		x = int( self.scale * x ) + self.x_offset
-		y = dc.Y - int( self.scale * y ) + self.y_offset
-		dc.SetPen( wx.Pen( color ) )
-		dc.SetBrush( wx.Brush( color ) )
-		dc.DrawCircle( x, y, 3 )
-	
-
-
-	def calc_scale_factor( self ) :
-		# get X and Y.
-		X = 0
-		Y = 0
-		for commands in self.pos_analyzer.commandss :
-			for cmd in commands :
-				if cmd.cmd_id == 0x8A :
-					X = max( X, cmd.x1 )
-					X = max( X, cmd.x2 )
-					Y = max( Y, cmd.y1 )
-					Y = max( Y, cmd.y2 )
-				else :
-					X = max( X, cmd.x )
-					Y = max( Y, cmd.y )
-
-		bmp = self.map_view.GetBitmap()
-		W = bmp.GetWidth()
-		H = bmp.GetHeight()
-
-		fx = W/X
-		fy = H/Y
-
-		self.scale = max( fx, fy )
-		print( "SCALE:", self.scale )
-		#self.scale = 0.045
-		#self.y_offset = -25
-	
-
-
-	def draw_positions( self, t ) :
-		#self.unit_view.show( self.kwr, scale=False ) # remove previous dots.
-		#self.map_view.SetBitmap( self.map_bmp )
-		#dc = wx.PaintDC( self.map_view )
-		#trans_brush = wx.Brush( wx.BLACK, style=wx.BRUSHSTYLE_TRANSPARENT )
-		#dc.SetBackground( trans_brush )
-		#dc.Clear()
-		bmp = wx.Bitmap( self.map_bmp )
-		dc = wx.MemoryDC( bmp )
-		null, dc.Y = dc.GetSize()
-
-		posa = self.pos_analyzer
-
-		# draw buildings
-		for i in range( 0, t ) :
-			commands = posa.structures[ i ]
-
-			for cmd in commands :
-				pid = cmd.player_id
-				player = self.kwr.players[ pid ]
-				if not player.is_player() :
-					continue
-				self.draw_building( dc, cmd.x, cmd.y, self.bldg_colors[pid] )
-
-
-		# Draw movement dots
-		for i in range( t-10, t ) :
-			commands = posa.commands[ i ]
-
-			for cmd in commands :
-				pid = cmd.player_id
-				player = self.kwr.players[ cmd.player_id ]
-				if not player.is_player() :
-					continue
-
-				if cmd.cmd_id == 0x8A : # wormhole
-					#print( "0x%08X" % cmd.cmd_id )
-					self.draw_dot( dc, cmd.x1, cmd.y1, self.colors[pid] )
-					self.draw_dot( dc, cmd.x2, cmd.y2, self.colors[pid] )
-				else :
-					self.draw_dot( dc, cmd.x, cmd.y, self.colors[pid] )
-
-		del dc
-		self.map_view.SetBitmap( bmp )
-
 
 
 	def digest_pos_analyzer( self, posa ) :
@@ -235,10 +256,12 @@ class PosViewer( wx.Frame ) :
 		self.digest_pos_analyzer( posa )
 
 		# drawing related things
-		self.map_view.show( kwr, scale=False, watermark=False )
-		self.calc_scale_factor()
+		self.minimap.posa = posa
+		self.minimap.kwr = kwr
+		self.minimap.show( kwr, scale=False, watermark=False )
+		self.minimap.calc_scale_factor()
 
-		self.map_bmp = self.map_view.GetBitmap()
+		#self.map_bmp = self.minimap.GetBitmap()
 		# At this point, the original state will be captured in the overlay.
 		#odc.Clear()
 
